af32ed31fb317eb539aeb69fd454fe0fb4e01a4f
bladechapman <bladechapman@gmail.com>
2014-08-06 01:24:52 -0700

    code cleaning

--- DrawingTest/ViewController.m
+++ DrawingTest/ViewController.m
@@ -25,7 +25,7 @@
     red = 0.0/255.0;
     green = 0.0/255.0;
     blue = 0.0/255.0;
-    brush = 1.0;
+    brush = 3.0;
     opacity = 1.0;
     
     _connectingLine = NO;
@@ -67,57 +67,35 @@
     UITouch *touch = [touches anyObject];
     CGPoint currentPoint = [touch locationInView:self.view];
 
-
-
-    //old style drawing
-//    UIGraphicsBeginImageContext(self.view.frame.size);
-//    [self.tempDrawImage.image drawInRect:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-//    CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
-//    CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), currentPoint.x, currentPoint.y);
-//    CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
-//    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 2.f);
-//    CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), 0.0, 1.0, 0.0, 1.0);
-//    CGContextSetBlendMode(UIGraphicsGetCurrentContext(), kCGBlendModeNormal);
-//
-//    CGContextStrokePath(UIGraphicsGetCurrentContext());
-//    self.tempDrawImage.image = UIGraphicsGetImageFromCurrentImageContext();
-//    [self.tempDrawImage setAlpha:opacity];
-//    UIGraphicsEndImageContext();
-
-
     //new style drawing
     [_points addObject:[NSValue valueWithCGPoint:[touch locationInView:self.view]]];
 
-    CGPoint subtract = CGPointMake(currentPoint.x - lastPoint.x, currentPoint.y - lastPoint.y);
-    CGFloat length = hypotf(subtract.x, subtract.y);
-    subtract.x /= length;
-    subtract.y /= length;
-    CGPoint perp = CGPointMake(-subtract.y, subtract.x);
-    
-    CGPoint A = CGPointMake(lastPoint.x + perp.x * brush/2, lastPoint.y + perp.y * brush/2);
-    CGPoint B = CGPointMake(lastPoint.x - perp.x * brush/2, lastPoint.y - perp.y * brush/2);
-    CGPoint C = CGPointMake(currentPoint.x + perp.x * brush/2, currentPoint.y + perp.y * brush/2);
-    CGPoint D = CGPointMake(currentPoint.x + perp.x * brush/2, currentPoint.y + perp.y * brush/2);
-    
-    if (_connectingLine) {
-        A = _prevC;
-        B = _prevD;
-    }
+    //Handle thickness
+//    CGPoint subtract = CGPointMake(currentPoint.x - lastPoint.x, currentPoint.y - lastPoint.y);
+//    CGFloat length = hypotf(subtract.x, subtract.y);
+//    subtract.x /= length;
+//    subtract.y /= length;
+//    CGPoint perp = CGPointMake(-subtract.y, subtract.x);
+//    
+//    CGPoint A = CGPointMake(lastPoint.x + perp.x * brush/2, lastPoint.y + perp.y * brush/2);
+//    CGPoint B = CGPointMake(lastPoint.x - perp.x * brush/2, lastPoint.y - perp.y * brush/2);
+//    CGPoint C = CGPointMake(currentPoint.x + perp.x * brush/2, currentPoint.y + perp.y * brush/2);
+//    CGPoint D = CGPointMake(currentPoint.x + perp.x * brush/2, currentPoint.y + perp.y * brush/2);
+//    
+//    if (_connectingLine) {
+//        A = _prevC;
+//        B = _prevD;
+//    }
 
     UIGraphicsBeginImageContext(self.view.frame.size);
     [self.tempDrawImage.image drawInRect:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
 
-//    CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
     NSMutableArray *smoothedPoints = [self calculateSmoothLinePoints];
     CGContextMoveToPoint(UIGraphicsGetCurrentContext(), [[smoothedPoints objectAtIndex:0] CGPointValue].x, [[smoothedPoints objectAtIndex:0] CGPointValue].y);
     for (int i = 1; i < [smoothedPoints count]; i++) {
         CGPoint addPoint = [[smoothedPoints objectAtIndex:i] CGPointValue];
         CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), addPoint.x, addPoint.y);
     }
-//    for(NSValue *i in smoothedPoints)
-//    {
-//        CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), [i CGPointValue].x, [i CGPointValue].y);
-//    }
     CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
     CGContextSetLineWidth(UIGraphicsGetCurrentContext(), brush);
     CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), red, green, blue, 1.0);
@@ -181,13 +159,9 @@
             for (NSUInteger j = 0; j < numberOfSegments; j++) {
                 CGPoint newPoint;
 
-                //formula from wiki
-                //(midPoint1 * powf(1 - t, 2)) + ((prev1.pos) * (2.0f * (1 - t) * t)) + (midPoint2 * (t * t))
+                //use quad curve equation to add interpolated points
                 newPoint.x = ((midPoint1.x * powf(1 - t, 2)) + (prev1.x * (2.0f * (1 - t) * t))) + (midPoint2.x * (t * t));
                 newPoint.y = ((midPoint1.y * powf(1 - t, 2)) + (prev1.y * (2.0f * (1 - t) * t))) + (midPoint2.y * (t * t));
-
-                //use quad curve equation to add interpolated points
-                //            newPoint.pos = ccpAdd(ccpAdd(ccpMult(midPoint1, powf(1 - t, 2)), ccpMult(prev1.pos, 2.0f * (1 - t) * t)), ccpMult(midPoint2, t * t));
 
                 [smoothedPoints addObject:[NSValue valueWithCGPoint:newPoint]];
 
